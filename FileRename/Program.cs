ab3230fcdca478df637966a0898b1e220ffce9ce
damagodon <nagahashi1710@gmail.com>
2022-02-17 22:39:31 +0900

    変更を加えました

--- FileRename/Program.cs
+++ FileRename/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading
 
 namespace CopyPractice
 {
@@ -14,9 +15,39 @@ namespace CopyPractice
         [STAThread]
         static void Main()
         {
+            // ThreadExceptionイベント・ハンドラの登録
+            Application.ThreadException += new
+              ThreadExceptionEventHandler(Application_ThreadException);
+
+            // UnhandledExceptionイベント・ハンドラの登録
+            Thread.GetDomain().UnhandledException += new
+              UnhandledExceptionEventHandler(Application_UnhandledException);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FileRename());
         }
+
+        public static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            ShowErrorMessage(e.Exception, "Application_ThreadExceptionによる例外通知です。");
+        }
+
+        public static void Application_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception ex = e.ExceptionObject as Exception;
+            if (ex != null)
+            {
+                ShowErrorMessage(ex, "Application_UnhandledExceptionによる例外通知です。");
+            }
+        }
+        public static void ShowErrorMessage(Exception ex, string extraMessage)
+        {
+            MessageBox.Show(extraMessage + " \n――――――――\n\n" +
+              "エラーが発生しました。開発元にお知らせください\n\n" +
+              "【エラー内容】\n" + ex.Message + "\n\n" +
+              "【スタックトレース】\n" + ex.StackTrace);
+        }
+
     }
 }
